0a883b14a8426572959e748b925c1181f67a93ab
WheatleyCODE <WheatleyCODE@users.noreply.github.com>
2021-11-10 17:37:57 +0300

    Fix styles, add boxshadow to sticky menu on scroll

--- components/layouts/Menus/StickyMenu.tsx
+++ components/layouts/Menus/StickyMenu.tsx
@@ -1,10 +1,27 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { Categories, Cart, Search } from '@components';
 import s from '@s/components/index.module.scss';
 
 export const StickyMenu: FC = () => {
+  const [className, setClassName] = useState('');
+
+  const scrollHandler = (e: any) => {
+    const top = e.target.documentElement.scrollTop;
+    if (top > 60) {
+      setClassName(s.shadow);
+    } else {
+      setClassName('');
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('scroll', scrollHandler);
+
+    return () => document.removeEventListener('scroll', scrollHandler);
+  }, []);
+
   return (
-    <div className={s.sticky}>
+    <div className={`${s.sticky} ${className}`}>
       <div className={s.parent}>
         <div className={s.width}>
           <div className={s.stickyMenu}>
