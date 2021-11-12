ae3c56324238f7ce58babd83357011e833178098
WheatleyCODE <WheatleyCODE@users.noreply.github.com>
2021-11-12 17:49:32 +0300

    Add spiner to search component

--- components/layouts/Menus/Search.tsx
+++ components/layouts/Menus/Search.tsx
@@ -1,12 +1,17 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { SearchInput, IconButton } from '@components/UI';
 import s from '@s/components/index.module.scss';
 
 export const Search: FC = () => {
+  const [search, setSearch] = useState(false);
+
   return (
     <div className={s.search}>
       <SearchInput />
-      <IconButton icon="fas fa-search" />
+      <IconButton
+        onClickHandler={() => setSearch((prev) => !prev)}
+        icon={search ? 'far fa-spinner-third rotation' : 'far fa-search'}
+      />
     </div>
   );
 };
