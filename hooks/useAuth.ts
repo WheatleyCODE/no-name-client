e7fd674f4e465165dc649e128375ef4bceb8943e
WheatleyCODE <WheatleyCODE@users.noreply.github.com>
2021-11-15 21:37:11 +0300

    Upgrade auth hook

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,13 +1,15 @@
 import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { useActions, useTypedSelector } from '@hooks';
 
 export const useAuth = () => {
   const { user, isAuth } = useTypedSelector((state) => state.user);
   const { checkAuthAC } = useActions();
+  const router = useRouter();
 
   useEffect(() => {
     checkAuthAC();
-  }, []);
+  }, [router]);
 
   return {
     user,
