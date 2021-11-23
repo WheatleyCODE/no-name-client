ceda5a4f7b63d2658ad922b9bc182f39281fc9f2
WheatleyCODE <WheatleyCODE@users.noreply.github.com>
2021-11-23 13:46:29 +0300

    Add mini cart disign

--- components/layouts/Menus/Cart/MiniCart.tsx
+++ components/layouts/Menus/Cart/MiniCart.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react';
 import s from '@s/components/index.module.scss';
+import { Button, Link } from '@components/UI';
+import { PathRoutes } from 'consts';
 
 interface MiniCartProps {
   onMouseHandlers: {
@@ -12,8 +14,19 @@ interface MiniCartProps {
 export const MiniCart: FC<MiniCartProps> = ({ onMouseHandlers }) => {
   return (
     <div {...onMouseHandlers} className={s.miniCart}>
-      <div className={s.cartMain}></div>
-      <div className={s.cartFooter}></div>
+      <div className={s.cartMain}>
+        {[1, 2, 3, 4, 5].map((el) => (
+          <div className={s.mainItem} key={el}></div>
+        ))}
+        <div className={s.cartLink}>
+          <Link href={PathRoutes.CART}>Перейти в Корзину</Link>
+        </div>
+      </div>
+      <div className={s.cartFooter}>
+        <div className={s.buttons}>
+          <Button className={s.orange}>Оформить заказ</Button>
+        </div>
+      </div>
     </div>
   );
 };
